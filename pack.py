d7a9a9fd0e6d22375d7356143942bce63dcf6242
Михаил Тапио <81358657+MikhailTapio@users.noreply.github.com>
2025-04-09 00:46:44 +0800

    :tada: Enhance pack script

--- pack.py
+++ pack.py
@@ -1,4 +1,7 @@
+import os
 from typing import List
+
+shall_not_delete: List[str] = []
 
 langs = ['en', 'zh']
 
@@ -10,33 +13,39 @@ from zipfile import ZipFile, ZIP_DEFLATED
 
 
 def should_skip(path: Path, patterns: List[str]) -> bool:
-    """判断某个路径是否需要排除"""
     for pat in patterns:
         if fnmatch.fnmatch(path.name, pat) or fnmatch.fnmatch(str(path.relative_to(Path('.'))), pat):
             return True
     return False
 
-def zip_lang(lang: str, exclude_logo: bool) -> str:
-    """递归打包目录，排除指定模式"""
-    lang_path = Path(lang)
+def zip_lang(lang_name: str, exclude_logo: bool) -> str:
+    lang_path = Path(lang_name)
     version_path = lang_path.joinpath('texts').joinpath('ru').joinpath('LC_MESSAGES').joinpath('version.info')
     with open(version_path, 'r', encoding='utf-8') as f:
         l10_v = f.readline()
     zip_infix = '.forum' if exclude_logo else ''
-    dst_zip = f'{l10_v}.{lang}.mod{zip_infix}.zip'
-    with ZipFile(dst_zip, "w", ZIP_DEFLATED) as zf:
-        for file in lang_path.rglob("*"):
-            if file.is_dir():
+    dst_zip = f'{l10_v}.{lang_name}.mod{zip_infix}.zip'
+    print(f'正在生成压缩包：{dst_zip}')
+    with ZipFile(dst_zip, 'w', ZIP_DEFLATED) as zf:
+        for child in lang_path.rglob('*'):
+            if child.is_dir():
                 continue
-            if should_skip(file, forum_patterns if exclude_logo else []):
-                print(f"已跳过：{file}")  # 调试用
+            if should_skip(child, forum_patterns if exclude_logo else []):
+                print(f'已跳过文件：{child}')
                 continue
-            zf.write(file, file.relative_to(lang_path))
+            zf.write(child, child.relative_to(lang_path))
+    shall_not_delete.append(dst_zip)
     return dst_zip
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     for lang in langs:
         # Full
-        print(f"已生成压缩包: {zip_lang(lang, False)}")
-        print(f"已生成压缩包: {zip_lang(lang, True)}")
+        print(f'已生成压缩包：{zip_lang(lang, False)}')
+        print(f'已生成压缩包：{zip_lang(lang, True)}')
+    for file in os.listdir('.'):
+        if not file.endswith('.zip'):
+            continue
+        if file not in shall_not_delete:
+            os.remove(file)
+            print(f'已删除压缩包：{file}')
     input('按回车键退出。')
