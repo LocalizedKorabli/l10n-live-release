097c05c6564f80f842abafb0990774237862cf43
Михаил Тапио <81358657+MikhailTapio@users.noreply.github.com>
2025-05-10 17:03:23 +0800

    :tada: Enhancement

--- pack.py
+++ pack.py
@@ -1,17 +1,36 @@
 import os
+import subprocess
+import shutil
+import fnmatch
+from pathlib import Path
+from zipfile import ZipFile, ZIP_DEFLATED
 from typing import List
 
 shall_not_delete: List[str] = []
 
-langs = ['en', 'zh']
+langs = ['en', 'es', 'zh', 'cht']
 
-forum_exclude_patterns = ['game_logo.svg', 'game_logo_static.svg']
+iscc_path = r'C:\Program Files (x86)\Inno Setup 6\ISCC.exe'
+
+forum_exclude_patterns = ['game_logo.svg', 'game_logo_alt.svg', 'game_logo_static.svg', 'game_logo_static_alt.svg', 'zzz_lk_ee_zjsj.mo', 'zzz_lk_ee_wws.mo']
+
 min_include_patterns = ['version.info', 'global.mo']
 
-import fnmatch
-from pathlib import Path
-from zipfile import ZipFile, ZIP_DEFLATED
+lang_base_paths = {
+    'en': r'D:\dev\wows\Korabli-LESTA-I18N',
+    'es': r'D:\dev\wows\Korabli-I18n-ES',
+    'zh': r'D:\dev\wows\Korabli-LESTA-L10N',
+    'cht': r'D:\dev\wows\Korabli-L10n-CHT'
+}
 
+installer_name_pattern = {
+    'zh': '澪刻汉化离线包-$game_version$-$l10n_version$.exe',
+    'cht': '澪刻繁體中文化離綫包-$game_version$-$l10n_version$.exe',
+    'en': 'LK-English-L10n-Offline-$game_version$-$l10n_version$.exe',
+    'es': 'LK-Español-Localización-$game_version$-$l10n_version$.exe'
+}
+
+shared_files_path = Path('Shared')
 
 def should_skip(path: Path, patterns: List[str]) -> bool:
     for pat in patterns:
@@ -19,15 +38,41 @@ def should_skip(path: Path, patterns: List[str]) -> bool:
             return True
     return False
 
+def _collect_lang(lang_name: str, l10n_path: Path, ee_path: Path, locale_config_path: Path):
+    print(f'正在收集{lang_name}语言的文件…')
+    base_target_path = Path('Localizations').joinpath(lang_name)
+    shutil.rmtree(base_target_path)
+    os.makedirs(base_target_path, exist_ok=True)
+    os.makedirs(base_target_path.joinpath('res_mods').joinpath('texts').joinpath('ru').joinpath('LC_MESSAGES'), exist_ok=True)
+    os.makedirs(base_target_path.joinpath('bin64'), exist_ok=True)
+    for file in os.listdir(l10n_path):
+        if 'version.info' in file or 'global.mo' in file:
+            shutil.copy(l10n_path.joinpath(file), base_target_path.joinpath('res_mods').joinpath('texts').joinpath('ru').joinpath('LC_MESSAGES').joinpath(file))
+    shutil.copytree(ee_path, base_target_path.joinpath('res_mods'), dirs_exist_ok=True)
+    shutil.copy(locale_config_path, base_target_path.joinpath('res_mods').joinpath('locale_config.xml'))
+    shutil.copytree(shared_files_path, base_target_path.joinpath('bin64'), dirs_exist_ok=True)
+
+def collect_lang(lang_name: str, base_path: Path):
+    _collect_lang(
+        lang_name=lang_name,
+        l10n_path=base_path.joinpath('Localizations').joinpath('latest'),
+        ee_path=base_path.joinpath('BuiltInMods').joinpath('LKExperienceEnhancement'),
+        locale_config_path=base_path.joinpath('Localizations').joinpath('locale_config.xml')
+    )
+
 def zip_lang(lang_name: str, zip_type: str) -> str:
-    lang_path = Path(lang_name)
+    lang_path = Path('Localizations').joinpath(lang_name).joinpath('res_mods')
     version_path = lang_path.joinpath('texts').joinpath('ru').joinpath('LC_MESSAGES').joinpath('version.info')
     with open(version_path, 'r', encoding='utf-8') as f:
-        l10_v = f.readline()
+        lines = f.readlines()
+        l10n_v = lines[0].strip() if len(lines) > 0 else ''
+        game_v = lines[1].strip() if len(lines) > 1 else ''
+
     is_min = zip_type == 'min'
     exclude_logo = zip_type == 'forum'
     zip_infix = '' if is_min else '.mod.forum' if exclude_logo else '.mod'
-    dst_zip = f'{l10_v}.{lang_name}{zip_infix}.zip'
+    game_v_prefix = (game_v + '.') if game_v else ''
+    dst_zip = f'Releases/{game_v_prefix}{l10n_v}.{lang_name}{zip_infix}.zip'
     print(f'正在生成压缩包：{dst_zip}')
     with ZipFile(dst_zip, 'w', ZIP_DEFLATED) as zf:
         for child in lang_path.rglob('*'):
@@ -46,13 +91,64 @@ def zip_lang(lang_name: str, zip_type: str) -> str:
     shall_not_delete.append(dst_zip)
     return dst_zip
 
+def pack_lang(lang_name: str) -> bool:
+    lang_path = Path('Localizations').joinpath(lang_name)
+    version_path = lang_path.joinpath('res_mods').joinpath('texts').joinpath('ru').joinpath('LC_MESSAGES').joinpath('version.info')
+
+    with open(version_path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+        l10n_v = lines[0].strip() if len(lines) > 0 else ''
+        game_v = lines[1].strip() if len(lines) > 1 else ''
+    
+    target_iss_file = f'lk_offline_installer_{lang_name}.iss'
+
+    if not os.path.exists(target_iss_file):
+        return False
+
+    with open(target_iss_file, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+
+    with open(target_iss_file, 'w', encoding='utf-8') as file:
+        for line in lines:
+            if line.strip().startswith('AppVersion='):
+                line = f'AppVersion={game_v}.{l10n_v}\n'
+            file.write(line)
+    print(f'正在生成{lang_name}语言的安装包…')
+    result = subprocess.run([iscc_path, target_iss_file], capture_output=True, text=True)
+
+    print("编译输出：")
+    print(result.stdout)
+
+    # Errors (if exist)
+    if result.stderr:
+        print("编译错误：")
+        print(result.stderr)
+        return False
+    os.rename('Output/mysetup.exe', f'Output/{installer_name_pattern[lang_name].replace("$l10n_version$", l10n_v).replace("$game_version$", game_v)}')
+    return True
+
+
 if __name__ == '__main__':
+    os.makedirs('Releases', exist_ok=True)
+    for file in os.listdir('Output'):
+        os.remove(f'Output/{file}')
     for lang in langs:
-        # Full
-        print(f'已生成压缩包：{zip_lang(lang, "min")}')
-        print(f'已生成压缩包：{zip_lang(lang, "forum")}')
-        print(f'已生成压缩包：{zip_lang(lang, "full")}')
-    for file in os.listdir('.'):
+        try:
+            # Full
+            collect_lang(lang_name=lang, base_path=Path(lang_base_paths[lang]))
+            print(f'已生成压缩包：{zip_lang(lang, "min")}')
+            print(f'已生成压缩包：{zip_lang(lang, "forum")}')
+            print(f'已生成压缩包：{zip_lang(lang, "full")}')
+            pack = pack_lang(lang)
+            if pack:
+                print(f'已生成{lang}语言的安装包。')
+            else:
+                print(f'{lang}语言的安装包生成失败')
+        except Exception as ex:
+            print('异常：')
+            print(ex)
+    for file in os.listdir('Releases'):
+        file = f'Releases/{file}'
         if not file.endswith('.zip'):
             continue
         if file not in shall_not_delete:
